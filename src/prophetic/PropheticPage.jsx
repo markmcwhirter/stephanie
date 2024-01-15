515d112ba64783df83ca61933b8a4b3789368f89
markmcwhirter <mark.mcwhirter@gmail.com>
2024-01-15 11:29:09 -0700

    attempt to embed youtube channel or playlist

--- src/prophetic/PropheticPage.jsx
+++ src/prophetic/PropheticPage.jsx
@@ -1,8 +1,17 @@
+import Iframe from 'react-iframe'
+
 export default function PropheticPage() {
     return (
-        <div id="propheticpage" className="torn-paper">
-            <div className="flex-container narrative styled-paragraph">
-            </div>
+        <div id="propheticpage" >
+            <Iframe url="http://www.youtube.com/embed/videoseries?list=UC2I1BWQUziEPw-RgKvvlhsg"
+                width="100%"
+                height="100%"
+                id="propheticFrame"
+                className=""
+                //display="initial"
+                //position="absolute"
+                frameborder="0" marginheight="0px" marginwidth="0px" allowfullscreen />
+
             <div id="homebar">
                 <a href="#titlepage">Home</a>
             </div>
