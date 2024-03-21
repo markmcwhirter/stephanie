b74c4381886c7b30f3b0426745c63fdf2b33fd83
markmcwhirter <mark.mcwhirter@gmail.com>
2024-03-20 17:49:31 -0700

    remove contact forms

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import * as React from "react";
 import BioPage from "./bio/BioPage";
 import BookPage from "./book/BookPage";
-import BlogPage from "./blog/BlogPage";
-import PropheticPage from "./prophetic/PropheticPage";
 import MediaPage from "./media/MediaPage";
 import ShoppingPage from "./shopping/ShoppingPage";
 
@@ -11,30 +9,27 @@ import { MGMProvider } from './util/MGMContextProvider';
 import MailchimpForm from "./util/MailchimpForm";
 import ContactContainer from "./contact/ContactContainer/ContactContainer";
 
-
-
 export default function App() {
 
   return (
     <>
       <MGMProvider>
         <div className="content" id="content">
-
           <div id="titlepage">
             <div className="flex-container">
+
+              <div className="flex-child">
               <div id="navbar1">
                 <a href="#titlepage">Home</a>
                 <a href="#biopage">About</a>
                 <a href="#bookpage">Books</a>
-                <a href="http://146.190.48.100/" target="_blank">Blog</a>
-                <a href="https://www.youtube.com/@StephanieMckittrick" target="_blank">Prophetic Ministry</a>
+                <a href="http://146.190.48.100/" target="_blank"  rel="noreferrer">Blog</a>
+                <a href="https://www.youtube.com/@StephanieMckittrick" target="_blank"  rel="noreferrer">Prophetic Ministry</a>
                 <a href="#mediapage">Media</a>
                 <a href="#shoppage">Shop</a>
               </div>
-              <div className="flex-child">
-                <img src="images/steph_cover.jpg" alt="" />
+                <img src="images/steph1b.jpg" alt="" className="image_sizer_large" />
               </div>
-
             </div>
           </div>
 
@@ -42,8 +37,6 @@ export default function App() {
           <BookPage></BookPage>
           <MediaPage></MediaPage>
           <ShoppingPage></ShoppingPage>
-          <ContactContainer></ContactContainer>
-          <Modal><MailchimpForm /></Modal>
         </div>
       </MGMProvider>
     </>
--- src/bio/BioPage.jsx
+++ src/bio/BioPage.jsx
@@ -3,15 +3,16 @@ export default function BioPage() {
         <div id="biopage" className="torn-paper">
             <div className="flex-container narrative styled-paragraph">
                 <div className="flex-child">
-                Welcome! I'm Stephanie McKittrick, a Christian life coach, counselor, author, and speaker with a passion for guiding women through life's challenges and into the transformative embrace of faith and healing.
+                <p>Welcome! I'm Stephanie McKittrick, a Christian life coach, counselor, author, and speaker with a passion for guiding women 
+                    through life's challenges and into the transformative embrace of faith and healing.</p>
 
-My journey has been one of remarkable resilience and redemption. Having faced the staggering adversity of 17 brain surgeries, 20 stomach surgeries, and numerous other trials, I understand firsthand the depths of suffering. However, amidst the darkness, I encountered a miraculous moment of divine intervention that forever altered the course of my life. From that pivotal encounter with God, I found purpose and strength to navigate through adversity with unwavering faith.
+                <p>My journey has been one of remarkable resilience and redemption. Having faced the staggering adversity of 17 brain surgeries, 20 stomach surgeries, and numerous other trials, I understand firsthand the depths of suffering. However, amidst the darkness, I encountered a miraculous moment of divine intervention that forever altered the course of my life. From that pivotal encounter with God, I found purpose and strength to navigate through adversity with unwavering faith.</p>
 
-One of the most profound challenges I've endured was overcoming the devastating loss of my baby girl. Her passing tested every fiber of my being, yet through the grace of God, I discovered the courage to grieve, heal, and find hope beyond the pain.
+                <p>One of the most profound challenges I've endured was overcoming the devastating loss of my baby girl. Her passing tested every fiber of my being, yet through the grace of God, I discovered the courage to grieve, heal, and find hope beyond the pain.</p>
 
-As a single mother to a beautiful daughter named Lexie, I've experienced the joys and challenges of parenthood firsthand. My love for Lexie fuels my dedication to empowering women and guiding them towards a life of purpose and fulfillment.
+                <p>As a single mother to a beautiful daughter named Lexie, I've experienced the joys and challenges of parenthood firsthand. My love for Lexie fuels my dedication to empowering women and guiding them towards a life of purpose and fulfillment.</p>
 
-My mission is to extend that same beacon of hope to others who are struggling. I am deeply committed to helping women break free from the shackles of abuse, addiction, and suffering, guiding them towards a path of healing and restoration in the loving arms of Jesus Christ.
+                <p>My mission is to extend that same beacon of hope to others who are struggling. I am deeply committed to helping women break free from the shackles of abuse, addiction, and suffering, guiding them towards a path of healing and restoration in the loving arms of Jesus Christ.</p>
                 </div>
 
                 <div className="flex-child">
